519e9b071f3beccf8b33678832455d83fcdd1eb2
Unknown <xwilarg@yahoo.fr>
2016-12-10 14:44:41 +0100

    Spawn ennemies.

---
+++ Assets/SpawnEnemies.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    [Tooltip("Prefab of the enemy to spawn.")]
+    public Rigidbody2D enemy;
+    [Tooltip("Intervalle between each enemies spawn in secondes.")]
+    public float intervalleSpawn;
+    private float iniTime;
+    // Use this for initialization
+    void Start()
+    {
+        iniTime = Time.fixedTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.fixedTime - iniTime > intervalleSpawn)
+        {
+            Rigidbody2D enemyClone = (Rigidbody2D)Instantiate(enemy, transform.position, transform.rotation);
+            iniTime = Time.fixedTime;
+        }
+    }
+}
