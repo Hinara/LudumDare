d2a100e08de7de8837b72da34971c2bb4b75f23f
Unknown <xwilarg@yahoo.fr>
2016-12-10 18:20:59 +0100

    Loot added.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour {
+    public int minCoin;
+    public int maxCoin;
+    public int minXp;
+    public int maxXp;
     public int life;
+    public Rigidbody2D coin;
+    public Rigidbody2D xp;
 	// Use this for initialization
 	void Start () {
 		
@@ -20,6 +26,22 @@ public class Character : MonoBehaviour {
         print(life);
         if (life < 0)
         {
+            int nbCoins = Random.Range(minCoin, maxCoin);
+            for (int i = 0; i < nbCoins; i++)
+            {
+                float randomX = Random.Range(0.0f, 2.0f) - 1.0f;
+                float randomY = Random.Range(0.0f, 2.0f) - 1.0f;
+                Vector2 posCoin = new Vector2(transform.position.x + randomX, transform.position.y + randomY);
+                Rigidbody2D coinClone = (Rigidbody2D)Instantiate(coin, posCoin, transform.rotation);
+            }
+            int nbXp = Random.Range(minXp, maxXp);
+            for (int i = 0; i < nbXp; i++)
+            {
+                float randomX = Random.Range(0.0f, 2.0f) - 1.0f;
+                float randomY = Random.Range(0.0f, 2.0f) - 1.0f;
+                Vector2 posXp = new Vector2(transform.position.x + randomX, transform.position.y + randomY);
+                Rigidbody2D xpClone = (Rigidbody2D)Instantiate(xp, posXp, transform.rotation);
+            }
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/slideAway.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slideAway : MonoBehaviour {
+    public float speed;
+    public float frottement;
+    private float angle;
+	// Use this for initialization
+	void Start () {
+		angle = Random.Range(0.0f, 2 * Mathf.PI);
+        Debug.Log(angle);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (speed > 0)
+        {
+            Vector2 newPos = new Vector2(transform.position.x + Mathf.Cos(angle), transform.position.y + Mathf.Sin(angle));
+            transform.position = newPos * speed;
+            speed -= frottement;
+        }
+        else
+            Destroy(gameObject);
+	}
+}
