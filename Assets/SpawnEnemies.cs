a492f5205ce4a86c97c2935460f1b9a9c52f3f5c
Unknown <xwilarg@yahoo.fr>
2016-12-10 15:43:26 +0100

    Added an enemy and fixed a bug.

--- Assets/SpawnEnemies.cs
+++ Assets/SpawnEnemies.cs
@@ -6,13 +6,13 @@ public class SpawnEnemies : MonoBehaviour
 {
     [Tooltip("Prefab of the enemy to spawn.")]
     public Rigidbody2D enemy;
+    public Rigidbody2D enemy2;
     [Tooltip("Intervalle between each enemies spawn in secondes.")]
     public float intervalleSpawn;
     private float iniTime;
     // Use this for initialization
     void Start()
     {
-        iniTime = Time.fixedTime;
     }
 
     // Update is called once per frame
@@ -20,7 +20,8 @@ public class SpawnEnemies : MonoBehaviour
     {
         if (Time.fixedTime - iniTime > intervalleSpawn)
         {
-            Rigidbody2D enemyClone = (Rigidbody2D)Instantiate(enemy, transform.position, transform.rotation);
+            int randomEnemy = Random.Range(1, 3);
+            Rigidbody2D enemyClone = (Rigidbody2D)Instantiate((randomEnemy == 1) ? (enemy) : (enemy2), transform.position, transform.rotation);
             iniTime = Time.fixedTime;
         }
     }
